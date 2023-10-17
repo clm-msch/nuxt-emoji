37245ed920b9ab1815ea563842ecc3ab3e9ee988
clement <58054176+clm-msch@users.noreply.github.com>
2023-10-17 11:17:05 +0200

    🧐 Fetch emoji data from json in vue component

--- components/HelloComponent.vue
+++ components/HelloComponent.vue
@@ -1,5 +1,30 @@
 <template>
     <div>
-        Hello world
+      <div v-for="emoji in emojis" :key="emoji.emoji" @click="selectEmoji(emoji)">
+        {{ emoji.emoji }}
+      </div>
     </div>
-</template>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        emojis: []
+      }
+    },
+    mounted() {
+      fetch('https://raw.githubusercontent.com/github/gemoji/master/db/emoji.json')
+        .then(response => response.json())
+        .then(data => {
+          this.emojis = data.map(emoji => ({ emoji: emoji.emoji, description: emoji.description }))
+        })
+        .catch(error => console.error(error))
+    },
+    methods: {
+      selectEmoji(emoji) {
+        console.log(emoji)
+      }
+    }
+  }
+  </script>
